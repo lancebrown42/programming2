5ed9ea9ec6d908888b9016e344b2ef13891dc460
Lance <lance.brown42@gmail.com>
2020-05-25 21:32:22 -0400

    assignment 1 complete

---
+++ SumOfNumbers/SumOfNumbers/frmMain.vb
@@ -0,0 +1,16 @@
+﻿
+'''Lance Brown
+'''5/25/20
+'''Program to take a single input and calculate the sum from 1 to the input/
+
+Public Class frmMain
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnNumbers_Click(sender As Object, e As EventArgs) Handles btnNumbers.Click
+        frmInput.ShowDialog()
+    End Sub
+End Class
