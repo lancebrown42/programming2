7714200d1bcc0d5045152e5569a4502ba380b67e
Lance <lance.brown42@gmail.com>
2020-06-28 16:02:29 -0400

    arrays

---
+++ RainfallStatistics/RainfallStatistics/frmMain.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtAvg = New System.Windows.Forms.TextBox()
+        Me.txtMin = New System.Windows.Forms.TextBox()
+        Me.txtMax = New System.Windows.Forms.TextBox()
+        Me.btnInput = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtTotal
+        '
+        Me.txtTotal.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(223, 104)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(377, 20)
+        Me.txtTotal.TabIndex = 1
+        '
+        'txtAvg
+        '
+        Me.txtAvg.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtAvg.Enabled = False
+        Me.txtAvg.Location = New System.Drawing.Point(223, 140)
+        Me.txtAvg.Name = "txtAvg"
+        Me.txtAvg.Size = New System.Drawing.Size(377, 20)
+        Me.txtAvg.TabIndex = 2
+        '
+        'txtMin
+        '
+        Me.txtMin.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtMin.Enabled = False
+        Me.txtMin.Location = New System.Drawing.Point(223, 179)
+        Me.txtMin.Name = "txtMin"
+        Me.txtMin.Size = New System.Drawing.Size(377, 20)
+        Me.txtMin.TabIndex = 3
+        '
+        'txtMax
+        '
+        Me.txtMax.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtMax.Enabled = False
+        Me.txtMax.Location = New System.Drawing.Point(223, 216)
+        Me.txtMax.Name = "txtMax"
+        Me.txtMax.Size = New System.Drawing.Size(377, 20)
+        Me.txtMax.TabIndex = 4
+        '
+        'btnInput
+        '
+        Me.btnInput.Location = New System.Drawing.Point(12, 341)
+        Me.btnInput.Name = "btnInput"
+        Me.btnInput.Size = New System.Drawing.Size(130, 36)
+        Me.btnInput.TabIndex = 5
+        Me.btnInput.Text = "Input Monthly Rainfall"
+        Me.btnInput.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(163, 341)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(130, 36)
+        Me.btnDisplay.TabIndex = 6
+        Me.btnDisplay.Text = "Display Stats"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(321, 341)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(130, 36)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(480, 341)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(130, 36)
+        Me.btnClose.TabIndex = 8
+        Me.btnClose.Text = "Exit"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(220, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Monthly Rainfall Statistics"
+        '
+        'txtInput
+        '
+        Me.txtInput.BackColor = System.Drawing.Color.White
+        Me.txtInput.Enabled = False
+        Me.txtInput.ForeColor = System.Drawing.Color.Black
+        Me.txtInput.Location = New System.Drawing.Point(15, 32)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(193, 249)
+        Me.txtInput.TabIndex = 10
+        Me.txtInput.Text = "Monthly Rainfall Input" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "--------------------------" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 422)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnInput)
+        Me.Controls.Add(Me.txtMax)
+        Me.Controls.Add(Me.txtMin)
+        Me.Controls.Add(Me.txtAvg)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Name = "frmMain"
+        Me.Text = "Rainfall Statistics"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtAvg As TextBox
+    Friend WithEvents txtMin As TextBox
+    Friend WithEvents txtMax As TextBox
+    Friend WithEvents btnInput As Button
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+End Class
---
+++ RainfallStatistics/RainfallStatistics/frmMain.vb
@@ -0,0 +1,79 @@
+﻿Public Class frmMain
+    Dim arrRainfall(12) As String
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim input As String = InputBox("Input monthly rainfall serparated by commas (eg. 3, 8, 6, 6, 4, 7, 5, 6, 3, 8, 2, 4, 3:")
+        If String.IsNullOrEmpty(input) Then
+            input = InputBox("Input required" & vbCrLf & "Input monthly rainfall serparated by commas (eg. 3, 8, 6, 6, 4, 7, 5, 6, 3, 8, 2, 4, 3:")
+        End If
+        For Each c As Char In input
+            If Not IsNumeric(c) And c <> "," And c <> "." Then
+                input = InputBox("Input monthly rainfall as numbers serparated by commas (eg. 3, 8, 6, 6, 4, 7, 5, 6, 3, 8, 2, 4, 3:")
+                Exit For
+            End If
+        Next
+        arrRainfall = SplitRecord(input)
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        txtInput.Text = "Monthly Rainfall Input" & vbCrLf & "---------------------------"
+        Dim dblTotal As Double
+        Dim arrNumbers() As Double = {}
+        Dim i As Int32 = 0
+        For Each entry In arrRainfall
+            txtInput.AppendText(vbCrLf & "Rainfall for " & MonthName(i + 1) & "= " & entry)
+
+            dblTotal += entry
+            Array.Resize(arrNumbers, arrNumbers.Length + 1)
+            arrNumbers(i) = CDbl(entry)
+            Debug.WriteLine(arrNumbers(i))
+            i += 1
+        Next
+        txtTotal.Text = "The total annual rainfall was " & dblTotal
+        txtAvg.Text = "The average monthly rainfall was " & (dblTotal / 12)
+        txtMin.Text = "The minimum monthly rainfall was " & arrNumbers.Min & " (" & MonthName(Array.IndexOf(arrNumbers, arrNumbers.Min) + 1) & ")"
+        txtMax.Text = "The maximum monthly rainfall was " & arrNumbers.Max & " (" & MonthName(Array.IndexOf(arrNumbers, arrNumbers.Max) + 1) & ")"
+
+    End Sub
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        For Each ctrl As Control In Controls
+            If TypeOf ctrl Is TextBox Then
+                ctrl.ResetText()
+            End If
+        Next
+    End Sub
+    Private Function SplitRecord(ByVal strRecord As String) As String()
+        Dim arrSub(5) As String
+        arrSub = strRecord.Split(New Char() {","c}, 12)
+
+        'weed out additional entries
+        If arrSub(arrSub.Length - 1).IndexOf(",") <> -1 Then
+            arrSub(5) = arrSub(11).Substring(0, arrSub(11).IndexOf(","))
+        End If
+        'trim whitespace
+        For i = 0 To arrSub.Length - 1
+
+            arrSub(i) = arrSub(i).Trim()
+
+        Next
+        If arrSub.Length < 12 Then
+            Array.Resize(Of String)(arrSub, 12)
+            For i = 0 To arrSub.Length - 1
+                If IsNothing(arrSub(i)) Then
+                    arrSub(i) = ""
+                End If
+            Next
+        End If
+
+        Return arrSub
+
+    End Function
+    'Private Sub InputBox1_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles InputBox1.KeyPress
+
+    'End Sub
+End Class
